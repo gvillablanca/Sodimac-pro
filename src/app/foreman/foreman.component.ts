60701a62b7bab9ebb3c8cc796fdff2efa0ed385f
gvillablanca <g.villalobo.m@gmail.com>
2018-04-04 12:46:05 -0300

    conexiones objetos

--- src/app/foreman/foreman.component.ts
+++ src/app/foreman/foreman.component.ts
@@ -10,6 +10,95 @@ import {mywork} from './../../../src/data/mywork.json';
 export class ForemanComponent implements OnInit {
 
   constructor() { }
+  Maestros = [
+   {
+     nombre: "Raul Cortez",
+     ciudad: "Santiago",
+     categoria: "Jardineria",
+     disponibilidad: "true"
+   },
+   {
+     nombre: "Edwart Elric",
+     ciudad: "Santiago",
+     categoria: "Construcción",
+     disponibilidad: "true"
+   },
+   {
+     nombre: "Marcos Ruiz",
+     ciudad: "Santiago",
+     categoria: "Jardineria",
+     disponibilidad: "true"
+   },
+   {
+     nombre: "Raul Cortez",
+     ciudad: "Santiago",
+     categoria: "Terraza",
+     disponibilidad: "true"
+   }
+ ]
+
+ 
+   proyectos = [
+     {
+       id: "1",
+       nombre : "proyecto terraza",
+       descripcion : "construcción de una terraza para un jardin",
+       disponibilidad : "ocupado",
+       direccion : "av siempre viva 1234",
+       ciudad : "santiago"
+     },
+     {
+       id: "2",
+       nombre : "proyecto cocina",
+       descripcion : "reconstruccion de cocina en un departamento",
+       disponibilidad : "disponible",
+       direccion : "av siempre viva 1234",
+       ciudad : "talca"
+     },
+     {
+       id: "3",
+       nombre : "proyecto terraza",
+       descripcion : "construcción de una terraza para un jardin",
+       disponibilidad : "ocupado",
+       direccion : "av italia 1234",
+       ciudad : "coquimbo"
+     },
+     {
+       id: "4",
+       nombre : "proyecto dormitorio",
+       descripcion : "construcción de un dormitorio en un segundo piso",
+       disponibilidad : "disponible",
+       direccion : "av siempre viva 1234",
+       ciudad : "coquimbo"
+     },
+     {
+       id: "5",
+       nombre : "proyecto terraza",
+       descripcion : "construcción de una terraza para un jardin",
+       disponibilidad : "disponible",
+       direccion : "av siempre viva 1234",
+       ciudad : "coquimbo"
+     }
+   ]
+
+   mywork= [
+     {
+       id: "1",
+       nombre : "proyecto terraza",
+       descripcion : "construcción de una terraza para un jardin",
+       disponibilidad : "en proceso",
+       direccion : "av lo que sea 1234",
+       ciudad : "santiago"
+     },
+     {
+       id: "5",
+       nombre : "proyecto jardineria",
+       descripcion : "redecoracion de jardin trasero de una parcela",
+       disponibilidad : "acabado",
+       direccion : "oscarito 2134",
+       ciudad : "coquimbo"
+     }
+   ]
 
   ngOnInit() {
 
@@ -26,6 +115,7 @@ export class ForemanComponent implements OnInit {
 
       function boton1 (){
         $('.table1').show();
+        $('.table2').hide();
         console.log(proyectos);        
         for(let i=0; i<proyectos.length; i++){
           let data = proyectos[i];
@@ -41,7 +131,6 @@ export class ForemanComponent implements OnInit {
         `);
 
         }
-        
       }
 
       function boton2(){
